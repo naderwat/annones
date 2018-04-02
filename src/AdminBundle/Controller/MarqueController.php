89a9bae94365d5493634831b32a4efe4b8575cd2
naderwat <nader.abdelkader2015@outlook.fr>
2018-04-02 06:36:08 +0100

    #02042018

--- src/AdminBundle/Controller/MarqueController.php
+++ src/AdminBundle/Controller/MarqueController.php
@@ -5,6 +5,10 @@ namespace AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use AdminBundle\Entity\Marque;
+use AdminBundle\Form\MarqueType;
 
 class MarqueController extends Controller
 {
@@ -22,10 +26,47 @@ class MarqueController extends Controller
 
 	}
 
-	public function addAction()
-	{
+	public function getUploadDir()
+    {
+        return 'uploads/logos/';
+    }
 
-		return $this->render('AdminBundle:Marque:add.html.twig');
+
+    protected function getUploadRootDir()
+    {
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+    }
+
+
+
+	public function addAction(Request $request)
+	{
+		$marque = new Marque();
+		$em = $this->getDoctrine()->getManager();
+		$form = $this->get('form.factory')->create(MarqueType::class,$marque);
+
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+			var_dump($_FILES);
+			die();
+
+			 $target_file = $this->getUploadDir() . basename($_FILES["adminbundle_pdocs"]['name']["filename"]);
+                 move_uploaded_file($_FILES["adminbundle_pdocs"]["tmp_name"]['filename'], $target_file);
+
+
+			$marque->setDateAjout(new \Datetime());
+			$marque->setDateEdit(new \Datetime());
+			$marque->setUser($this->getUser());
+			$marque->setLogo($_FILES['adminbundle_pdocs']['name']['filename'])
+			$em->persist($marque);
+			$em->flush();
+			$session = new Session();
+			 $session->getFlashBag()->add('notice', 'Marque ajoutée avec succés !');
+             return $this->redirectToRoute('admin_marques');
+
+
+		}
+		return $this->render('AdminBundle:Marque:add.html.twig',array('form'=>$form->createView()));
 
 	}
 
--- src/AdminBundle/Form/MarqueType.php
+++ src/AdminBundle/Form/MarqueType.php
@@ -5,6 +5,16 @@ namespace AdminBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType; 
+
 
 class MarqueType extends AbstractType
 {
@@ -13,7 +23,10 @@ class MarqueType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titre')->add('logo')->add('dateAjout')->add('dateEdit')->add('user')        ;
+        $builder->add('titre',TextType::class)
+                 ->add('logo',FileType::class,array('label'=>'Logo'))
+                 ->add('valider',SubmitType::class)
+                    ;
     }
     
     /**
